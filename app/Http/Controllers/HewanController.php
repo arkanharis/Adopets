41bbb96451be59411c8790fc1f8b85c5f0d97772
irfriza <irfnriza@gmail.com>
2024-05-14 12:18:25 +0700

    membuat cotroller dan model hewan

---
+++ app/Http/Controllers/HewanController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hewan;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HewanController extends Controller
+{
+    public function create()
+    {
+        return view('hewan.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'jenis' => 'required|string|max:255',
+            'usia' => 'required|integer',
+            'jenis_kelamin' => 'required|string|max:255',
+            'kontak' => 'required|string|max:255',
+            'deskripsi' => 'nullable|string',
+        ]);
+
+        Hewan::create($request->all());
+
+        return redirect()->route('hewan.index')->with('success', 'Hewan berhasil ditambahkan.');
+    }
+
+    public function index()
+    {
+        $hewan = Hewan::all();
+        return view('hewan.index', compact('hewan'));
+    }
+}
